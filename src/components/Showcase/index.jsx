913baa5a272543d0ec6b48cc845731b2b93314d7
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-03-19 19:33:33 +0000

    chore: initial commit

---
+++ src/components/Showcase/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useThemeConfig} from '@docusaurus/theme-common';
+
+export const Showcase = ({users}) => {
+    const {isDarkTheme} = useThemeConfig()
+    return users.map(user => (
+        <a href={user.infoLink} key={user.infoLink}>
+            <img
+                src={isDarkTheme ? user.darkImage : user.lightImage}
+                alt={user.caption}
+                title={user.caption}
+            />
+        </a>
+    ))
+}
