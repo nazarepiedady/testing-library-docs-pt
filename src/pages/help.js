ba694cadac3a8b6c28a3a809d46350a5a686a8e9
Nazaré da Piedade <nazarepiedady@gmail.com>
2023-03-21 14:28:25 +0000

    docs(pt): translate `src/pages/help.js`

--- src/pages/help.js
+++ src/pages/help.js
@@ -25,28 +25,28 @@ export default function Help(props) {
 
   const supportLinks = [
     {
-      content: `Ask a question on [Stack Overflow](https://stackoverflow.com/questions/tagged/react-testing-library)`,
+      content: `Faça um pergunta na [Stack Overflow](https://stackoverflow.com/questions/tagged/react-testing-library)`,
       title: 'Stack Overflow',
     },
     {
       content:
-        'Discuss issues with community members on [Spectrum](https://spectrum.chat/testing-library)',
+        'Discuta problemas com os membros da comunidade na [Spectrum](https://spectrum.chat/testing-library)',
       title: 'Spectrum',
     },
     {
-      content: `Chat on [Discord](https://discord.gg/testing-library)`,
+      content: `Converse na [Discord](https://discord.gg/testing-library)`,
       title: 'Discord',
     },
     {
-      content: `Stay up to date by following the [blog](${props.config.baseUrl}blog)`,
-      title: 'Blog',
+      content: `Mantenha-te atualizado seguindo o [blogue](${props.config.baseUrl}blog)`,
+      title: 'Blogue',
     },
     {
-      content: `Browse [Learning Material](/docs/learning)`,
-      title: 'Resources',
+      content: `Dê uma vista de olhos no [Material de Aprendizado](/docs/learning)`,
+      title: 'Recursos',
     },
     {
-      content: `Get support on [GitHub](${siteConfig.customFields.repoUrl})`,
+      content: `Receba suporte na [GitHub](${siteConfig.customFields.repoUrl})`,
       title: 'GitHub',
     },
   ]
@@ -61,7 +61,7 @@ export default function Help(props) {
         <div className="mainContainer documentContainer postContainer helpContainer">
           <div>
             <header>
-              <h1>Need help?</h1>
+              <h1>Precisas de ajuda?</h1>
             </header>
             <GridBlock
               contents={supportLinks.slice(0, 3)}
@@ -74,13 +74,13 @@ export default function Help(props) {
               align="left"
             />
             <section>
-              <h2>Buy a Course</h2>
+              <h2>Compre um Curso</h2>
               <p>
-                Learn how to test JavaScript with{' '}
+                Aprenda a testar a JavaScript com o{' '}
                 <ExternalLink href="https://kentcdodds.com">
                   Kent C. Dodds
                 </ExternalLink>
-                , the creator of Testing Library, on{' '}
+                , o criador da Testing Library, na{' '}
                 <ExternalLink href="https://testingjavascript.com">
                   TestingJavaScript.com
                 </ExternalLink>
@@ -89,36 +89,31 @@ export default function Help(props) {
                 <ExternalLink href="https://testingjavascript.com">
                   <img
                     width="500"
-                    alt="TestingJavaScript.com Learn the smart, efficient way to test any JavaScript application."
+                    alt="TestingJavaScript.com Aprenda a maneira inteligente e eficiente de testar qualquer aplicação de JavaScript."
                     src={`${baseUrl}img/testingjavascript.jpg`}
                   />
                 </ExternalLink>
               </div>
             </section>
             <header className="postHeader">
-              <h1>Want to help?</h1>
+              <h1>Queres ajudar?</h1>
             </header>
             <p>
-              Thanks! The Testing Library maintainers are happy to maintain this
-              library along with you, the community. We aren't looking for
-              funding, but we need everyone to pitch in to make this project and
-              community successful and improve tests for everyone in the long
-              run.
+              Obrigado! Os responsáveis da Testing Library estão felizes em manter esta biblioteca juntamente convosco, a comunidade. Nós não estamos a procura de financiamento, mas precisamos de todos à contribuir para fazer deste projeto e comunidade bem-sucedida e melhorar os testes para todos a longo prazo.
             </p>
             <p>
-              Please consider helping us answer community questions and update
-              documentation content via the help links above. You can also help
-              support{' '}
+
+              Considere ajudar-nos a responder as questões da comunidade e atualizar o conteúdo da comunidade através das ligações de ajuda acima. Tu também podes apoiar o{' '}
               <ExternalLink href="https://kentcdodds.com">
                 Kent C. Dodds
               </ExternalLink>{' '}
-              financially by purchasing his{' '}
+              financeiramente comprando os seus{' '}
               <ExternalLink href="https://kentcdodds.com/courses">
-                courses
+                cursos
               </ExternalLink>{' '}
-              or{' '}
+              ou{' '}
               <ExternalLink href="https://kentcdodds.com/workshops/">
-                remote workshops
+                oficinas remotas
               </ExternalLink>
               .
             </p>
